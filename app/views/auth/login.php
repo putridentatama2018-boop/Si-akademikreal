ff8e280a9848b1f038c0b0669a2797041eb39641
Putri <putridentatama2018@gmail.com>
2026-09-13 13:08:40 +0700

    Mengubah halaman login di fitur

--- app/views/auth/login.php
+++ app/views/auth/login.php
@@ -56,7 +56,7 @@ unset($_SESSION['flash']);
 
 <div class="login-box">
 
-    <h1>Login Sistem Akademik</h1>
+    <h2>Login Sistem Akademik - FITUR</h2>
 
     <?php if ($flash): ?>
         <div class="alert">
